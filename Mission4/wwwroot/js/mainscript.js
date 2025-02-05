faf865ab752b35eedc39527172fb027d8e168821
bybeejac000 <jake.bybee@bybeemail.com>
2025-02-05 14:28:06 -0700

    first commit

---
+++ Mission4/wwwroot/js/mainscript.js
@@ -0,0 +1,38 @@
+﻿//Check if the entered number is correct
+function validate() {
+    var hrs = $("#hrs").val();
+    hrs = parseInt(hrs);
+
+
+    if (hrs < 1 || hrs > 10 || isNaN(hrs)) {
+        $("#hrs").val("");
+
+        alert("Please enter a number between 1 and 10");
+        return false;
+    }
+
+    else {
+        return true;
+    }
+
+}
+
+
+
+//Do the rate calculation
+function calculate(valid) {
+
+
+    var hrs = $("#hrs").val();
+    hrs = parseInt(hrs);
+
+    var rate = $("#rate").val();
+    rate = parseInt(rate);
+
+    if (valid) {
+        const total = rate * hrs;
+
+        $("#total").val(total);
+    }
+
+}
