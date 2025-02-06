36845ea357c242345aa88d098636e5c02fc79f60
bybeejac000 <jake.bybee@bybeemail.com>
2025-02-05 17:01:08 -0700

    last one

--- Mission4/Program.cs
+++ Mission4/Program.cs
@@ -2,6 +2,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 
+
+//setup the server file
 var app = builder.Build();
 
 app.UseHttpsRedirection();
